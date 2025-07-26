9f664b192d690c1257578af0e8f170815ad56505
Lutho Mngqibisa <luthoyrn@gmail.com>
2025-07-26 20:20:31 +0200

    initialization for database integration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 
-#db = SQLAlchemy()
-#migrate = Migrate() 
+db = SQLAlchemy()
+migrate = Migrate() 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
-  # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///test.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///test.db')
 
-  # db.init_app(app)
-  # migrate.init_app(app, db)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes import main
     app.register_blueprint(main)
