432f6e18581d5451c03eacb306f2639fe12a8df1
Lutho Mngqibisa <luthoyrn@gmail.com>
2025-07-28 01:14:22 +0200

    Refactor app to integrate Supabase for code storage and management; remove SQLAlchemy and related files

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,21 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from supabase import create_client, Client
 from dotenv import load_dotenv
 import os
 
-db = SQLAlchemy()
-migrate = Migrate() 
 load_dotenv()
+# Initialize Supabase client
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_SERVICE_KEY") 
+supabase: Client = create_client(supabase_url, supabase_key)
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     CORS(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///test.db')
-
-    db.init_app(app)
-    migrate.init_app(app, db)
 
     from .routes import main
     app.register_blueprint(main)
--- backend/app/models.py
+++
@@ -1,7 +0,0 @@
-#from . import db
-
-#class Todo(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    content = db.Column(db.String(200), nullable=False)
-#    completed = db.Column(db.Integer, default=0)
-#    date_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
